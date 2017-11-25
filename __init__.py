54b37d2c6b3ca9ab3b2bc25ba304475714c6e224
Ed Guy <edguy@eguy.org>
2017-11-25 04:10:19 +0000

    fix errors in paramters.

--- __init__.py
+++ __init__.py
@@ -9,8 +9,7 @@ import fileinput
 import os
 import time
 
-from asterisk.ami import AMIClient
-from asterisk.ami import EventListener
+from asterisk.ami import AMIClient, EventListener, AutoReconnect
 from pprint import pprint
 
 from os.path import dirname
@@ -28,21 +27,28 @@ class AsteriskCliSkill(MycroftSkill):
     # constructor calls MycroftSkill's constructor to build data structures. 
     def __init__(self):
         super(AsteriskCliSkill, self).__init__(name="AsteriskCliSkill")
-        self.loggedin=false
+        self.loggedin=False
+        self.last="No One"
 
     # runtime initialize instance for use. 
     def initialize(self):
         self.load_data_files(dirname(__file__))
+	h=self.settings.get("host","127.0.0.1")
+	try:
+	    p=int(self.settings.get("port",5038))
+	except Exception:
+            p=5038
 
-        self.client = AMIClient(address=self.settings.get("host","127.0.0.1"),port=p=self.settings.get("port",5038))
-        future = self.client.login(username=self.settings.get('username'),secret=self.settings.get('password'))
+        self.client = AMIClient(address=h,port=p)
+        future = self.client.login(username=self.settings.get('username','pi'),secret=self.settings.get('password','password'))
         if future.response.is_error():
-            LOGGER.debug(str(future.response))
+            LOGGER.debug("AsteriskCliSkill login fail: "+str(future.response))
             return
-        AutoReconnect(self.client)
+        self.loggedin=True
+        self.autoconnect=AutoReconnect(self.client)
             
-        self.client.add_event_listener(EventListener(on_event=handle_cli, white_list='Newstate', ChannelStateDesc='Ringing'))
-        LOGGER.debug("AsteriskCliSkill Listening")
+        self.client.add_event_listener(EventListener(on_event=self.handle_cli, white_list='Newstate', ChannelStateDesc='Ringing'))
+        LOGGER.debug("AsteriskCliSkill Listening host: "+h+" port set to: "+str(p))
 
         # what was that last call? 
         #        last_call_intent = IntentBuilder("LastCallIntent").\
@@ -58,8 +64,21 @@ class AsteriskCliSkill(MycroftSkill):
 
     # tear down connection
     def stop(self):
-        LOGGER.debug("AsteriskCliSkill Stopped")
-        self.client.logoff()
+        LOGGER.debug("AsteriskCliSkill Stopping...")
+	if self.loggedin:
+	    try:
+		del self.autoconnect
+            except Exception:
+                LOGGER.debug("AsteriskCliSkill Exception handled 1")
+	if self.loggedin:
+	    try:
+                self.client.logoff(self.finalstop)
+            except Exception:
+                LOGGER.debug("AsteriskCliSkill Exception handled 2")
+		pass
+    def finalstop(self):
+	del self.client
+
 
 def create_skill():
     return AsteriskCliSkill()
